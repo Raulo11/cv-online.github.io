a319a96130cfcf6bff9bd2ce7bbb0721f549d556
Raulo.11 <raulo.ferrari@gmail.com>
2023-02-02 17:52:01 -0300

    repositorio cv online

---
+++ despedida.js
@@ -0,0 +1,8 @@
+document.getElementById('botondespedida').onclick = function () {
+    console.log("Capturamos el evento click")
+    document.getElementById("despedida").innerHTML = "Gracias por tu tiempo"
+}
+document.getElementById('formacion').onclick = function () {
+    console.log("Capturamos el evento consultar")
+    document.getElementById("formacionacade").innerHTML = "09/2005 IES Columela | Cádiz FP de Grado Medio en Comercio y Marketing " 
+}
